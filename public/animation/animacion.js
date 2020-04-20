a407b14ab01e2f10166feb5f9bc2451acb414c9a
RubelPerez <rubel.daniel.perez@gmail.com>
2020-04-20 18:24:50 +0200

    Fixed all mobile changes

--- public/animation/animacion.js
+++ public/animation/animacion.js
@@ -12,6 +12,21 @@ function mostrarScroll () {
     }
 }
 window.addEventListener('scroll',mostrarScroll);
+//derecha
 
+let animadoDerecha = document.querySelectorAll(".animadoDerecha");
+
+function mostrarScrollDerecha () {
+    let scrolltopDerecha = document.documentElement.scrollTop;
+    for(var i=0;i<animadoDerecha.length;i++)
+    { let alturaAnimadoDerecha = animadoDerecha[i].offsetTop;
+        if(alturaAnimadoDerecha-500<scrolltopDerecha)
+        {
+            animadoDerecha[i].style.opacity=1;
+            animadoDerecha[i].classList.add('mostrarDerecha')
+        }
+    }
+}
+window.addEventListener('scroll',mostrarScrollDerecha);
 
 //Header banner
