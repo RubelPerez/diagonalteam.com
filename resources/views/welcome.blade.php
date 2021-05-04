243e4e24fce8e829376e5058f7eed39f6f95d662
BONET <bonetprogramacion@gmail.com>
2021-05-04 13:40:31 +0200

    Images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo2.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo2.ico') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description"
         content="Start up digital con nucleo en Barcelona, España especializado en la programación, desarrollo y diseño web y en el posicionamiento SEO de estas">
